3fe6638c7123800b4def3346e871438a78e8d5ae
irivera2116 <irivera2116@GMAIL.COM>
2023-04-19 20:24:07 -0400

    Added code to JavaScript, Update README.md

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,77 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//Variables
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+function generatePassword() {
+  //Sets character parameters
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+
+  //If parameters are not met
+  while(confirmLength <= 8 || confirmLength >= 128) {
+    alert("Password length must be between 8-128 characters Try again");
+    var confirmLength = (prompt("How many characters would you like your password to contain?"));
+    } 
+
+        // Determine parameters of password 
+        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+          // Loop if answer is outside the parameters 
+          while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+            var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+            var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+            var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+            var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+          }
+        
+        var passwordCharacters = []
+      
+        if (confirmSpecialCharacter) {
+          passwordCharacters = passwordCharacters.concat(specialChar)
+        }
+    
+        if (confirmNumericCharacter) {
+          passwordCharacters = passwordCharacters.concat(number)
+        }
+          
+        if (confirmLowerCase) {
+          passwordCharacters = passwordCharacters.concat(alphaLower)
+        }
+    
+        if (confirmUpperCase) {
+          passwordCharacters = passwordCharacters.concat(alphaUpper)
+        }
+    
+          console.log(passwordCharacters)
+    
+          // Empty string to be filled based on for loop selecting random characters from the array
+          var randomPassword = ""
+          
+          for (var i = 0; i < confirmLength; i++) {
+            randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+            console.log(randomPassword)
+          }
+          return randomPassword;
+    }
+    
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,6 +80,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Password Generator Starter Code
+
+# User Story
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+![alt text](/Develop/PassGen.png)
+
+# Acceptance Criteria
+I used the other lessons learned during the JavaScripts lessons to develop some of the code in order to develop this generator. In addition a web developer on github had code which helped clarify the project a bit more. 
+
+# Link to Deployed Site
+
+
+# Links that Helped
+https://github.com/TiffanyCasey/Password-Generator
+
+https://www.youtube.com/watch?v=x4HUaiazDes
+
